c72db2452793d8b2ba626b44ec0936c5d8058447
Veronica Reingold <veronika.reingold@gmail.com>
2025-09-01 20:11:26 -0400

    New Action

--- wics-frontend/astro.config.mjs
+++ wics-frontend/astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://UWinWiCS.github.io',
+  base: '/wics-web',
   vite: {
     plugins: [tailwindcss()]
   },
