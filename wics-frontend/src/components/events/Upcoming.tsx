2861a8e5de541a05a2827bc04c689e59f582a4df
Veronica Reingold <veronika.reingold@gmail.com>
2025-12-25 19:49:40 -0500

    Swapped out old events

--- wics-frontend/src/components/events/Upcoming.tsx
+++ wics-frontend/src/components/events/Upcoming.tsx
@@ -14,38 +14,11 @@ function Upcoming() {
             <div className="flex flex-wrap justify-center gap-10 items-stretch w-full px-4 sm:px-0 mt-10">
                 <Event
                     icon={faCircleInfo}
-                    title="Spin & Win Info Booth"
-                    date="September 3rd, 2025"
-                    time="12:00 - 2:00 PM"
-                    location="Lambton Tower Patio"
-                    description="Learn more about our club and events! Spin our prize wheel to get cute merch!"
-                    className=""
-                />
-                <Event
-                    icon={faCircleInfo}
-                    title="Tips & Tricks: Upper Year Q&A"
-                    date="September 5th, 2025"
-                    time="2:00 - 4:00 PM"
-                    location="Erie Hall (Room TBD)"
-                    description="Want a head start to help you plan your university career? Or maybe just some tips to help you transition into uni life? Come chat with the upper year WiCS!"
-                    className=""
-                />
-                <Event
-                    icon={faCookieBite}
-                    title="Bits & Bytes: Bake Sale"
-                    date="September 9th, 2025"
-                    time="12:00 - 2:00 PM"
-                    location="CAW"
-                    description="Support our club and fund future events by getting yourself a little treat! Our talented team also made some cute crochet items!"
-                    className=""
-                />
-                <Event
-                    icon={faCookieBite}
-                    title="Mingle & Munch: Welcome Party"
-                    date="September 9th, 2025"
-                    time="5:30 - 7:30 PM"
-                    location="Erie Hall (Room TBD)"
-                    description="Celebrate the start of the year with us! Free snacks and drinks will be provided, just bring your awesome self and make some new friends!"
+                    title="More events coming soon!"
+                    date="TBD"
+                    time="TBD"
+                    location="TBD"
+                    description="We'll see you in the new year for more fun events!"
                     className=""
                 />
             </div>
